acfecddff05766536e3a04cd8a45725ddfac4225
DoorlessLlama71 <113698791+DoorlessLlama71@users.noreply.github.com>
2023-09-07 11:59:01 -0500

    created dropdown menu on slider
    started pulling inputs

--- app/src/main/java/com/example/calculatorapprajan/MainActivity.java
+++ app/src/main/java/com/example/calculatorapprajan/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.calculatorapprajan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void calculate(View v){
+        EditText firstInputTN = findViewById(R.id.firstInputTextNumber);
+        int firstInput = Integer.parseInt(firstInputTN.getText().toString());
+        EditText secondInputTN = findViewById(R.id.secondInputTextNumber);
+        int secondInput = Integer.parseInt(secondInputTN.getText().toString());
+    }
+
 }
