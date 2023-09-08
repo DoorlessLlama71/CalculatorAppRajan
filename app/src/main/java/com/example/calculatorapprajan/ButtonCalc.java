7123e3631bc8cf7c970822cc181c5dc08ea13bce
Anirudh R <anirudhrajan07@gmail.com>
2023-09-07 19:48:04 -0500

    finished functionality
    added another screen(normal button calculator)

---
+++ app/src/main/java/com/example/calculatorapprajan/ButtonCalc.java
@@ -0,0 +1,14 @@
+package com.example.calculatorapprajan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ButtonCalc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button_calc);
+    }
+}
--- app/src/main/java/com/example/calculatorapprajan/MainActivity.java
+++ app/src/main/java/com/example/calculatorapprajan/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,11 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void calculate(View v){
+    public void calculate(View v) {
         EditText firstInputTN = findViewById(R.id.firstInputTextNumber);
-        int firstInput = Integer.parseInt(firstInputTN.getText().toString());
         EditText secondInputTN = findViewById(R.id.secondInputTextNumber);
+        TextView enterTV = findViewById(R.id.resultTextView);
+        Spinner operationSpin = findViewById(R.id.operationSelectorSpinner);
+        String operation = operationSpin.getSelectedItem().toString();
+        System.out.println(operation);
+        int firstInput = Integer.parseInt(firstInputTN.getText().toString());
         int secondInput = Integer.parseInt(secondInputTN.getText().toString());
+        if (operation.equals("+")) {
+            enterTV.setText("" + (0.0 + firstInput + secondInput));
+        } else if (operation.equals("-")) {
+            enterTV.setText("" + (0.0 + firstInput - secondInput));
+        } else if (operation.equals("*")) {
+            enterTV.setText("" + (1.0 * firstInput * secondInput));
+        } else {
+            enterTV.setText("" + (1.0 * firstInput / secondInput));
+        }
+    }
+    public void switchToButtonCalc(View v){
+
     }
 
 }
