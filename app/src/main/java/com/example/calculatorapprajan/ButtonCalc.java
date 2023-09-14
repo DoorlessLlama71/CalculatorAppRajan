554a8d1ad258e67aa1efed69cc35da78cda58961
Anirudh R <anirudhrajan07@gmail.com>
2023-09-13 23:10:10 -0500

    Started new button page functionality

--- app/src/main/java/com/example/calculatorapprajan/ButtonCalc.java
+++ app/src/main/java/com/example/calculatorapprajan/ButtonCalc.java
@@ -1,9 +1,13 @@
 package com.example.calculatorapprajan;
 
+import static java.lang.Integer.parseInt;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class ButtonCalc extends AppCompatActivity {
@@ -13,13 +17,61 @@ public class ButtonCalc extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_button_calc);
     }
+    public void switchToNormalCalc(View v){
+        //switch back to regular calculator screen
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
     public void numberOperationButtonInput(View v) {
         double result = 1.0;
         
     }
+    String typingNumber="";
+    double num1=0;
+    double num2=0;
+    String tempOperation="+";
 
     public void calculateButton(View v) {
-        double result = 1.0;
+        //performs the mathematical concepts using buttons
+        Button input = (Button)v;
+        String inputString = input.getText().toString();
+        System.out.println(inputString);
+        TextView outputTV = findViewById(R.id.outputTextView);
 
+         if (inputString.equals("Clear")) {
+            num1=0;
+            outputTV.setText("");
+        }
+        else if (inputString.equals("+")||inputString.equals("-")||inputString.equals("*")||inputString.equals("/")||inputString.equals("=")) {
+            try {
+                num2 = Integer.parseInt(typingNumber);
+                typingNumber="";
+            }
+            catch(Exception e){
+
+            }
+
+            if(tempOperation.equals("+")){
+                num1+=num2;
+            }
+            else if(tempOperation.equals("-")){
+                num1-=num2;
+            }
+            else if(tempOperation.equals("*")){
+                num1=num1*num2;
+            }
+            else if(tempOperation.equals("/")){
+                num1=num1/num2;
+            }
+            if(inputString.equals("=")){
+                outputTV.setText("" + num1);
+            }
+            else {
+                tempOperation = inputString;
+            }
+        }
+        else{
+            typingNumber+=inputString;
+        }
     }
 }
--- app/src/main/java/com/example/calculatorapprajan/MainActivity.java
+++ app/src/main/java/com/example/calculatorapprajan/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.calculatorapprajan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -16,16 +18,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //accessing text boxes
         firstInputTN = findViewById(R.id.firstInputTextNumber);
         secondInputTN = findViewById(R.id.secondInputTextNumber);
         operationSpin = findViewById(R.id.operationSelectorSpinner);
+        /*
+        operationSpin.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                operationSpin = findViewById(R.id.operationSelectorSpinner);
+                String operation = operationSpin.getSelectedItem().toString();
+                if (operation.equals("square root") || operation.equals("cube root")) {
+                    firstInputTN.setHint("Single Input");
+                    secondInputTN.setHint("Leave Blank(NC)");
+                    secondInputTN.setText("");
+                }
+            }
+        });
+        */
     }
     public void calculate(View v) {
+        //performs the mathematical concepts and outputs it
         TextView enterTV = findViewById(R.id.resultTextView);
         String operation = operationSpin.getSelectedItem().toString();
         System.out.println(operation);
         int firstInput = Integer.parseInt(firstInputTN.getText().toString());
         int secondInput = Integer.parseInt(secondInputTN.getText().toString());
+        if (operation.equals("square root") || operation.equals("cube root")) {
+            firstInputTN.setHint("Single Input");
+            secondInputTN.setHint("Leave Blank(NC)");
+            secondInputTN.setText("");
+        }
         if (operation.equals("+")) {
             enterTV.setText("" + (0.0 + firstInput + secondInput));
         } else if (operation.equals("-")) {
@@ -38,18 +61,10 @@ public class MainActivity extends AppCompatActivity {
             enterTV.setText("" + (1.0 * firstInput / secondInput));
         }
     }
-    public void checkSpinner(View v){
-
-            operationSpin = findViewById(R.id.operationSelectorSpinner);
-            String operation = operationSpin.getSelectedItem().toString();
-            if (operation.equals("square root") || operation.equals("cube root")) {
-                firstInputTN.setHint("Single Input");
-                secondInputTN.setHint("Leave Blank(NC)");
-                secondInputTN.setText("");
-            }
-    }
     public void switchToButtonCalc(View v){
-
+        //switches the screen to the button calculator
+        Intent intent = new Intent(this, ButtonCalc.class);
+        startActivity(intent);
     }
 
 }
